c4374aa58ccdf9774c205e2ad8b02d2d3fa44e79
Claudia Gutierrez <gutierrez.cp21@yahoo.com>
2019-04-06 11:04:58 -0400

    more

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,24 @@
+var name;
+var role;
+var startDate = null;
+var rate = null;
 
+$("#submit").on("click", function(event) {
+  event.preventDefault();
+
+var name = $("#name-input").val().trim();
+var role = $("#role-input").val().trim();
+var startDate = $("#startdate-input").val().trim();
+var rate = $("#rate-input").val().trim();
+
+// Console log each of the user inputs to confirm we are receiving them
+console.log(name);
+console.log(role);
+console.log(startDate);
+console.log(rate);
+
+// Output all of the new information into the relevant HTML sections
+$("#name-display").text(name);
+$("#role-display").text(email);
+$("#date-display").text(age);
+$("#months-worked-display").text(comment);
